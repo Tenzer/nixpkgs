0e9ab7d9398b98c4a086683a785fc1115421997c
Jeppe Fihl-Pearson <jeppe@memrise.com>
2020-10-01 09:54:43 +0100

    Bump version of openweathermap-exporter

--- openweathermap-exporter/default.nix
+++ openweathermap-exporter/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "openweathermap-exporter";
-  version = "2020-09-30";
+  version = "2020-10-01";
 
   src = fetchFromGitHub {
     owner = "Tenzer";
     repo = "openweathermap-exporter";
-    rev = "7e6c8c3b218a3dc854c783ad5a3cea73a4655033";
-    sha256 = "0pw7v50a82nvmf46hg79pmnw77yiz3w0qvi5rz4mxjy35dy9hjd3";
+    rev = "5653b84cfb401ea2d36f87f75ec5ab5d6f51631f";
+    sha256 = "167d512hj11mdj8g1lijgzy2g4n55s25ghls7iblm69vi6vxpqdx";
   };
 
   modSha256 = "1nmmpd9f3aisgl5frwzylw3341vnd2qwhd4syr8wbj748ca9xgqj";
