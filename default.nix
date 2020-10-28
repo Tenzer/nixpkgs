538759de8a3129611702f0613f85f7be7bcb0435
Jeppe Fihl-Pearson <jeppe@tenzer.dk>
2020-10-28 16:44:24 +0000

    Remove NextDNS package, it's available in stock nixpkgs for 20.09

--- default.nix
+++ default.nix
@@ -2,6 +2,5 @@ self: super:
 
 {
   homehub-metrics-exporter = super.callPackage ./homehub-metrics-exporter { };
-  nextdns = super.callPackage ./nextdns { };
   openweathermap-exporter = super.callPackage ./openweathermap-exporter { };
 }
